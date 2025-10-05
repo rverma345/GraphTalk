92790a14733fd108a5fc0e23da69b05e953d7152
rverma345 <vermaron9@gmail.com>
2025-10-05 10:25:50 +0530

    streaming backend added

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,10 +1,30 @@
 import streamlit as st
+from langgraph_backend import Chatbot
+from langchain_core.messages import HumanMessage
 user_input=st.chat_input("Type here:")
 
-messages=[]
+# message_history=[] # this list will always get reset when we press enter so we need st.session_state (history that persists 
+#st.session_state
+config={'configurable':{'thread_id':'thread_1'}}
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+# loadint the covnersation history
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
 
 if user_input:
+    #adding the message to message_history
+    st.session_state['message_history'].append({'role':'user','content':user_input})
     with st.chat_message("user"):
         st.text(user_input)
-    with st.chat_message("ai"):
-        st.text(user_input)
+    
+    response=Chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=config)
+    ai_message=response['messages'][-1].content
+    
+    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
+    with st.chat_message("assistant"):
+        st.text(ai_message)
