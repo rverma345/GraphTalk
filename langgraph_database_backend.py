b4923217b914987ce6c12625a17064571d89ae54
rverma345 <vermaron9@gmail.com>
2025-10-16 23:25:58 +0530

    recent updates on the project

--- langgraph_database_backend.py
+++ langgraph_database_backend.py
@@ -1,54 +1,39 @@
-from langgraph.graph import StateGraph,START,END
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-from typing import TypedDict,Annotated,List
-from langgraph.graph.message import BaseMessage,add_messages
-from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
 import sqlite3
-
 
 load_dotenv()
 
-llm=ChatOpenAI()
+llm = ChatOpenAI()
 
-#defining the state
 class ChatState(TypedDict):
-    messages: Annotated[list[BaseMessage],add_messages]
-
-# defining the chat node function
+    messages: Annotated[list[BaseMessage], add_messages]
 
 def chat_node(state: ChatState):
-    message=state['messages']
-    response=llm.invoke(message).content
-    return {"messages":[response]}
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
 
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+# Checkpointer
+checkpointer = SqliteSaver(conn=conn)
 
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
 
-conn=sqlite3.connect(database='chatbot.db',check_same_thread=False)
-# checkpointer
-checkpointer=SqliteSaver(conn=conn)
+chatbot = graph.compile(checkpointer=checkpointer)
 
-# graph inilialization
-graph=StateGraph(ChatState)
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
 
-# nodes
-graph.add_node('chat_node',chat_node)
+    return list(all_threads)
 
-
-#adding edges 
-
-graph.add_edge(START,'chat_node')
-graph.add_edge('chat_node',END)
-
-
-# compiling the graph 
-
-Chatbot=graph.compile(checkpointer=checkpointer)
-
-CONFIG={'configurable':{'thread_id':'thread-2'}}
-response=Chatbot.invoke(
-    {'messages': [HumanMessage(content='what is my name')]},
-    config=CONFIG
-)
-print(response)
--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from langgraph_backend import Chatbot
-from langchain_core.messages import HumanMessage, AIMessage
+from langgraph_database_backend import chatbot, retrieve_all_threads
+from langchain_core.messages import HumanMessage
 import uuid
 
 # **************************************** utility functions *************************
@@ -20,7 +20,7 @@ def add_thread(thread_id):
         st.session_state['chat_threads'].append(thread_id)
 
 def load_conversation(thread_id):
-    state = Chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
+    state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
     # Check if messages key exists in state values, return empty list if not
     return state.values.get('messages', [])
 
@@ -33,7 +33,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
 
@@ -80,20 +80,25 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    #CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 
-     # first add the message to message_history
-    with st.chat_message("assistant"):
-        def ai_only_stream():
-            for message_chunk, metadata in Chatbot.stream(
-                {"messages": [HumanMessage(content=user_input)]},
-                config=CONFIG,
-                stream_mode="messages"
-            ):
-                if isinstance(message_chunk, AIMessage):
-                    # yield only assistant tokens
-                    yield message_chunk.content
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
 
-        ai_message = st.write_stream(ai_only_stream())
+    # first add the message to message_history
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+        )
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
