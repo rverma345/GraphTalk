b7887b38dc6227f21cf7c107781fd2675c57e0d7
rverma345 <vermaron9@gmail.com>
2025-10-20 23:48:30 +0530

    new changes

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -102,3 +102,5 @@ if user_input:
         )
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    # here we change some things to make use of database
+    
